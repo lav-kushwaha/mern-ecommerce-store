5646ea9f9c2de71e011dc14d0aacbe4c4ad7978d
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-06 15:37:43 +0530

    admin view

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/button';
+import { AlignJustify } from 'lucide-react';
 
 const AdminHeader = () => {
   return <header className='flex items-center justify-between px-4 py-3 bg-background border-b'>
-    <Button>
-      
+     <Button className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className='sr-only'>Toggle Menu</span>
     </Button>
+    <div className='flex flex-1 justify-end'>
+      <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">Logout</Button>
+    </div>
   </header>
 }
 
--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { ChartNoAxesCombined } from 'lucide-react';
+import React, { Fragment } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function MenuItems(){
+  
+}
 
 const AdminSidebar = () => {
-  return (
-    <div>Admin Side bar</div>
-  )
+  const navigate = useNavigate();
+  return <Fragment>
+    <aside className='hidden w-64 flex-col border-r bg-background p-6 lg:flex'>
+      <div className='flex items-center gap-2 cursor-pointer' onClick={()=>navigate("/admin/dashboard")}>
+          <ChartNoAxesCombined size={30} />
+          <h1 className='text-xl font-bold'>Admin Pannel</h1>
+      </div>
+      </aside>
+  </Fragment>
 }
 
 export default AdminSidebar;
