ca09cd9cf14df5fb6cc66b81fee2e89debed5540
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-06 16:26:49 +0530

    Admin Sidebar

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,9 +1,41 @@
 import { ChartNoAxesCombined } from 'lucide-react';
+import { BadgeCheck, LayoutDashboard, ShoppingBasket } from "lucide-react"
 import React, { Fragment } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { adminSidebarMenuItems } from 'src/config';
+
+export const adminSidebarMenuItems = [
+    {
+        id:'dashboard',
+        label: 'Dashboard',
+        path : '/admin/dashboard',
+        icon: <LayoutDashboard/>
+    },
+    {
+        id:'products',
+        label: 'Products',
+        path : '/admin/products',
+        icon: <ShoppingBasket/>
+
+    },
+    {
+        id:'orders',
+        label: 'Products',
+        path : '/admin/orders',
+        icon: <BadgeCheck/>
+    },
+]
 
 function MenuItems(){
-  
+    const navigate = useNavigate();
+    return <nav className='mt-8 flex-col flex gap-2'>
+    {
+      adminSidebarMenuItems.map(menuItem=><div key={menuItem.id} onClick={()=>navigate(menuItem.path)} className='flex items-center gap-2 rounded-md px-3 py-2'>
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+      </div>)
+    }
+  </nav>
 }
 
 const AdminSidebar = () => {
@@ -14,6 +46,7 @@ const AdminSidebar = () => {
           <ChartNoAxesCombined size={30} />
           <h1 className='text-xl font-bold'>Admin Pannel</h1>
       </div>
+      <MenuItems/>
       </aside>
   </Fragment>
 }
