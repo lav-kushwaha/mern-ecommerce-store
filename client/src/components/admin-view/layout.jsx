49b333a35feff3e703134ee8b5145cd8d3b4dd8a
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-13 16:43:40 +0530

    admin-view layout

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -7,12 +7,10 @@ const AdminLayout = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className='flex min-h-screen w-full '>
-        {/*Admin side bar */}
         <AdminSidebar open={openSidebar} setOpen={setOpenSidebar}/>
           <div className='flex flex-1 flex-col'>
-            {/*Admin header*/}
             <AdminHeader setOpen={setOpenSidebar}/>
-            <main className='flex-1 flex bg-muted/40 p-4 md:p-6'>
+            <main className='flex-1 flex-col flex bg-muted/40 p-4 md:p-6'>
              <Outlet/>
             </main>
           </div>
