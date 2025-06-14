0d833e7e12b04768b50dcacf52d79b982c4343f0
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-14 21:08:17 +0530

    admin-view/header

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Button } from '../ui/button';
 import { AlignJustify } from 'lucide-react';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../../store/auth-slice';
+import { toast } from 'sonner';
 
 const AdminHeader = ({setOpen}) => {
+  const dispatch = useDispatch();
+  function handleLogout(){
+    dispatch(logoutUser()).then((data)=>{
+      console.log(data);
+       if(data?.payload?.success){
+        toast.success(data?.payload?.message)
+       }
+    })
+  }
   return (
     <header className='flex items-center justify-between px-4 py-3 bg-background border-b'>
      <Button onClick={()=>setOpen(true)} className="lg:hidden sm:block cursor-pointer">
@@ -10,7 +22,7 @@ const AdminHeader = ({setOpen}) => {
         <span className='sr-only'>Toggle Menu</span>
     </Button>
     <div className='flex flex-1 justify-end'>
-      <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">Logout</Button>
+      <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow cursor-pointer" onClick={()=>handleLogout()} >Logout</Button>
     </div>
   </header>
   )
