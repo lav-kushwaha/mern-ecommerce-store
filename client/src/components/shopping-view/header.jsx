c588bea1a9893d5c278a344868f092b17fa01b87
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-15 12:56:46 +0530

    shopping view header

---
+++ client/src/components/shopping-view/header.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { HousePlug, Menu, ShoppingCart } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
+import { Button } from '../ui/button';
+import { useSelector } from 'react-redux';
+import { shoppingViewHeaderMenuItems } from '../../config';
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { Avatar, AvatarFallback } from '../ui/avatar';
+
+function MenuItems(){
+  return <nav className='flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row'>
+      {
+        shoppingViewHeaderMenuItems.map(MenuItems=><Link className='font-medium hover:text-gray-700' key={MenuItems.id} to={MenuItems.path}>{MenuItems.label}</Link>)
+      }
+  </nav>
+}
+
+function HeaderRightContent(){
+  return <div className='flex lg:items-center lg:flex-row flex-col gap-4 '>
+    <Button variant="outline" size="icon" className="cursor-pointer">
+     <ShoppingCart className='w-6 h-6'/>
+     <span className='sr-only'>User cart</span>
+    </Button>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+      <Avatar className="bg-black">
+        <AvatarFallback className="bg-black text-white font-extrabold">
+          LAV
+        </AvatarFallback>
+      </Avatar>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+
+  </div>
+}
+
+function ShoppingHeader() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+
+        {/*For responsive design*/}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden cursor-pointer">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            {/* <HeaderRightContent /> */}
+          </SheetContent>
+        </Sheet>
+
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className="hidden lg:block">
+          {isAuthenticated?
+          <div>
+            <HeaderRightContent/>
+          </div>:null
+          }
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default ShoppingHeader;
