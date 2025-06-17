2973672a560c531216b91653687e8a090fca52df
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-17 23:22:34 +0530

    auth

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -3,7 +3,7 @@ import { Navigate, useLocation } from "react-router-dom";
 function CheckAuth({ isAuthenticated, user, children }) {
   const location = useLocation();
 
-  console.log(isAuthenticated,user);
+  // console.log(isAuthenticated,user);
   
   
   // 1. If NOT authenticated and trying to access protected routes, redirect to login
