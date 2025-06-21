dd38a796796b0c42750606e3c0df74faf8662042
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-21 22:10:58 +0530

    Updated cloudinary code

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,22 +2,21 @@ const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 async function uploadImageToCloudinary(file) {
-    const result = await cloudinary.uploader.upload(file,{
-        resource_type:'auto'
-    });
-    return result;
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: 'auto'
+  });
+  return result;
 }
 
-//multer to get file from req or from client side.
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB
 });
 
-module.exports = {upload,uploadImageToCloudinary}
+module.exports = { cloudinary, upload, uploadImageToCloudinary };
