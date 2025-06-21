ca14515f2344911d78588064e120cbb8b41fd8db
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-21 22:10:25 +0530

    products routes

--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -4,11 +4,10 @@ const { upload } = require('../../helpers/cloudinary');
 
 const router = express.Router();
 
-router.post('/upload-image',upload.single("my_file"),handleImageUpload);
+router.post('/upload-images', upload.array('my_file', 5), handleImageUpload);
 router.post('/add',addProduct);
 router.put('/edit/:id',editProduct);
 router.delete('/delete/:id',deleteProduct);
 router.get('/get',fetchAllProducts);
-
 
 module.exports = router;
