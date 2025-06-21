675ed45180f557295de1bed21c47ffec552301e7
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-21 22:12:42 +0530

    Created cart model

---
+++ server/models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  images: {
+    type: [String],
+    required: true,
+    validate: [arr => arr.length > 0, 'At least one image is required.']
+  },
+  title: { 
+    type: String, 
+    required: true
+   },
+  description: { 
+    type: String, 
+    required: true
+   },
+  category: { 
+    type: String,
+    required: true 
+  },
+  brand: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  salePrice: { 
+    type: Number,
+    required: true 
+  },
+  totalStock: { 
+    type: Number, 
+    required: true 
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.models.Product || mongoose.model('Product', ProductSchema);
