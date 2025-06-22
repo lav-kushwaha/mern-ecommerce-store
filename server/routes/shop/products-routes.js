7b84b40c02233d7c498e73d25058e71fdc1b350d
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-22 21:41:41 +0530

    products-routes

--- server/routes/shop/products-routes.js
+++ server/routes/shop/products-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getFilteredProducts } = require('../../controller/shop/products-controllers');
+const { getFilteredProducts,getProductDetails} = require('../../controller/shop/products-controllers');
 
 const router = express.Router();
 
 router.get("/get",getFilteredProducts);
+router.get("/get/:id",getProductDetails);
 
 module.exports = router;
