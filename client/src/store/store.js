ad6d05fc02e710e7b07374f337402601bd35a827
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-23 19:24:10 +0530

    Store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import {authReducer} from './auth-slice/index.js'
 import adminProductsSlice from './admin/products-slice'
 import shopProductsSlice from './shop/products-slice/'
+import shopCartSlice from "./shop/cart-slice";
+
 
 const store  = configureStore({
     reducer:{
         auth:authReducer,
         adminProduct:adminProductsSlice,
-        shopProducts:shopProductsSlice
+        shopProducts:shopProductsSlice,
+        shopCart: shopCartSlice,
+
     }
 })
 
