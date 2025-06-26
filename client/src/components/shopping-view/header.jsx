a91036e3b26fd91809befc5f305fca985d75f5d5
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-26 19:52:15 +0530

    shopping view header

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -25,7 +25,7 @@ function MenuItems() {
         <Link
           key={item.id}
           to={item.path}
-          className="font-medium hover:text-gray-500 transition-colors"
+          className="font-medium text-[18px] hover:text-gray-500 transition-colors"
         >
           {item.label}
         </Link>
@@ -127,7 +127,7 @@ function ShoppingHeader() {
   const { isAuthenticated } = useSelector((state) => state.auth);
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white shadow-md">
-      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+      <div className="flex h-17 items-center justify-between px-4 md:px-6">
         {/* Brand Logo */}
         <Link to="/shop/home" className="flex items-center gap-2 ">
           <HousePlug className="h-6 w-6" />
