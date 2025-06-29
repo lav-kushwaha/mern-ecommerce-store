2642e05eea8de85eafd2eeb955aa16ee6a2f4b68
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-30 01:02:42 +0530

    shopping view order

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../components/ui/table'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+      <Card>
+        <CardHeader>
+            <CardTitle>All Orders</CardTitle>
+        </CardHeader>
+         <CardContent>
+            <Table>
+            <TableHeader>
+                <TableRow>
+                <TableHead>Order ID</TableHead>
+                <TableHead>Order Date</TableHead>
+                <TableHead>Order Status</TableHead>
+                <TableHead>Order Price</TableHead>
+                <TableHead>
+                   <span className='sr-only'>Details</span>
+                </TableHead>
+                </TableRow>
+            </TableHeader>
+             <TableBody>
+               <TableRow>
+                    <TableCell>123456</TableCell>
+                    <TableCell>21/23/2001</TableCell>
+                    <TableCell>In Process</TableCell>
+                    <TableCell>200k</TableCell>
+                    <TableCell>
+                        <Button>
+                            View Details
+                        </Button>
+                    </TableCell>
+                </TableRow> 
+             </TableBody>  
+            </Table>
+        </CardContent>
+     </Card>
   )
 }
 
