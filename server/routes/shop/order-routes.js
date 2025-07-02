725e790c054b971c1db17dc6d4cfc83e34711ebe
Lav Kushwaha <lavkumar1224@gmail.com>
2025-07-02 23:22:33 +0530

    order routes

---
+++ server/routes/shop/order-routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createOrder, capturePayment } = require('../../controller/shop/order-controller');
+
+const router = express.Router();
+
+router.post('/create', createOrder);
+router.post('/capture', capturePayment);
+
+module.exports = router;
