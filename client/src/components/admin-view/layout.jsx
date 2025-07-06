cddf5975a16d20a809fb25f39ee7475be65748b8
Lav Kushwaha <lavkumar1224@gmail.com>
2025-07-06 21:51:04 +0530

    admin view

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import AdminSidebar from './sidebar';
 import AdminHeader from './header';
+import ShoppingFooter from '../shopping-view/footer';
 
 const AdminLayout = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
@@ -10,9 +11,10 @@ const AdminLayout = () => {
         <AdminSidebar open={openSidebar} setOpen={setOpenSidebar}/>
           <div className='flex flex-1 flex-col'>
             <AdminHeader setOpen={setOpenSidebar}/>
-            <main className='flex-1 flex-col flex bg-muted/40 p-4 md:p-6'>
+            <main className='flex-1 flex-col flex bg-white p-4 md:p-6'>
              <Outlet/>
             </main>
+            <ShoppingFooter/>
           </div>
     </div>
   )
