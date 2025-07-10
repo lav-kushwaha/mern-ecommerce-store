4b6fb9479f0a8e94ed9478368e96d2c1ca365b95
Lav Kushwaha <lavkumar1224@gmail.com>
2025-07-10 23:11:23 +0530

    order-routes

--- server/routes/admin/order-routes.js
+++ server/routes/admin/order-routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllOrdersOfAllUser, getOrderDetailsForAdmin } = require('../../controller/admin/orders-controller');
+const { getAllOrdersOfAllUser, getOrderDetailsForAdmin, updateOrderStatus } = require('../../controller/admin/orders-controller');
 
 const router = express.Router();
 
 router.get('/get',getAllOrdersOfAllUser);
 router.get('/details/:id',getOrderDetailsForAdmin);
+router.put('/update/:id',updateOrderStatus);
+
 
 
 module.exports = router;
