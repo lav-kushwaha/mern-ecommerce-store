1969c3a83bba0f88af37432204b61ef1eba99f8c
Lav Kushwaha <lavkumar1224@gmail.com>
2025-07-18 02:55:10 +0530

    star-rating

--- client/src/components/common/star-rating.jsx
+++ client/src/components/common/star-rating.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Button } from "../ui/button";
 import { StarIcon } from "lucide-react";
 
 const StartRatingComponent = ({ review = 0 }) => {
   return (
-    <div className="flex items-center mb-2">
+    <div className="flex items-center gap-1 mb-2">
       {[1, 2, 3, 4, 5].map((i) => (
         <StarIcon
           key={i}
-          className={`w-5 h-5 ${
+          className={`w-5 h-5 drop-shadow-sm transition ${
             i <= review.rating ? "text-yellow-400" : "text-gray-300"
           }`}
-          fill={i <= review.rating ? "currentColor" : "none"}
+          fill={i <= review.rating ? "#facc15" : "none"}
+          strokeWidth={1.5}
         />
       ))}
     </div>
